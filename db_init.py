0417c7e63b19873a129bcbd3ebdd35e76537d5d7
Sıla Küçüknane <silakcknn@gmail.com>
2022-12-09 21:12:05 +0300

    Implemented flask functions, created html files

--- db_init.py
+++ db_init.py
@@ -5,34 +5,138 @@ import pandas as pd
 INIT = [
 
     """
-    CREATE TABLE IF NOT EXISTS player (
-        player_id SERIAL PRIMARY KEY NOT NULL
+    CREATE TABLE IF NOT EXISTS team(
+        team_id INTEGER NOT NULL,
+        PRIMARY KEY (team_id)
     )
     """,
 
     """
+    CREATE TABLE IF NOT EXISTS player(
+        player_id SERIAL NOT NULL,
+        player_name VARCHAR(200) NOT NULL UNIQUE,
+        date_of_birth VARCHAR(200),
+        height INTEGER,
+        weight INTEGER,
+        overall_rating INTEGER,
+        potential_rating INTEGER,
+        best_position VARCHAR(10),
+        best_overall_rating INTEGER,
+        value INTEGER,
+        wage INTEGER,
+        player_image_url VARCHAR(200),
+        team_id INTEGER NOT NULL,
+        nationality VARCHAR(30),
+        PRIMARY KEY (player_id),
+        FOREIGN KEY (team_id)
+            REFERENCES team (team_id)
+            ON UPDATE CASCADE ON DELETE SET NULL
+    )
+    """,
+
+    """
+    CREATE TABLE IF NOT EXISTS player_attacking(
+        attacking_id SERIAL NOT NULL,
+        player_id SERIAL NOT NULL UNIQUE,
+        crossing INTEGER,
+        finishing INTEGER,
+        heading_accuracy INTEGER,
+        short_passing INTEGER,
+        volleys INTEGER,
+        PRIMARY KEY (attacking_id),
+        FOREIGN KEY (player_id)
+            REFERENCES player (player_id)
+            ON UPDATE CASCADE ON DELETE SET NULL 
+    )
+    """,
+
+    """
+    CREATE TABLE IF NOT EXISTS player_profile(
+        profile_id SERIAL NOT NULL ,
+        player_id SERIAL,
+        preferred_foot VARCHAR(80),
+        weak_foot INTEGER,
+        skill_moves INTEGER,
+        international_reputations INTEGER,
+        work_rate VARCHAR(80),
+        body_type VARCHAR(80),
+        PRIMARY KEY (profile_id),
+        FOREIGN KEY (player_id)
+            REFERENCES player (player_id)
+            ON UPDATE CASCADE ON DELETE CASCADE
+    )
+
+    """,
+    
+    """
+    CREATE TABLE IF NOT EXISTS player_skills(
+        skill_id  SERIAL NOT NULL ,
+        player_id SERIAL,
+        dribbling INTEGER,
+        curve INTEGER,
+        fk_accuracy INTEGER,
+        long_passing INTEGER,
+        ball_control INTEGER,
+        PRIMARY KEY (skill_id),
+        FOREIGN KEY (player_id)
+            REFERENCES player (player_id)
+            ON UPDATE CASCADE ON DELETE CASCADE
+    )
+    """,
+
+    """CREATE TABLE IF NOT EXISTS player_power(
+                            power_id SERIAL PRIMARY KEY,
+                            strength INTEGER,
+                            long_shots INTEGER,
+                            shot_power INTEGER,
+                            jumping INTEGER,
+                            stamina INTEGER,
+                            player_id SERIAL,
+                            FOREIGN KEY (player_id) 
+                                REFERENCES player (player_id)
+                                ON UPDATE CASCADE
+                                ON DELETE CASCADE
+    )""",
+
+    """CREATE TABLE IF NOT EXISTS player_movement(
+                            movement_id SERIAL PRIMARY KEY,
+                            reactions_id INTEGER,
+                            balance INTEGER,
+                            acceleration INTEGER,
+                            sprint_speed INTEGER,
+                            agility INTEGER,
+                            player_id SERIAL,
+                            FOREIGN KEY (player_id) 
+                                REFERENCES player (player_id)
+                                ON UPDATE CASCADE
+                                ON DELETE CASCADE
+    )""",
+
+    """
     CREATE TABLE IF NOT EXISTS player_goalkeeping (
-        goalkeeping_id  SERIAL PRIMARY KEY NOT NULL,
-        player_id       INT, 
+        goalkeeping_id  SERIAL NOT NULL,
+        player_id       SERIAL, 
         diving          INT,
         handling        INT,
         kicking         INT,
         positioning     INT,
-        reflexes        INT,  
+        reflexes        INT, 
+        PRIMARY KEY (goalkeeping_id), 
         FOREIGN KEY (player_id) REFERENCES player(player_id) ON DELETE CASCADE ON UPDATE CASCADE
     )
     """,
 
     """
     CREATE TABLE IF NOT EXISTS player_mentality (
-        mentality_id    SERIAL PRIMARY KEY NOT NULL,
-        player_id       INT, 
+        mentality_id    SERIAL NOT NULL,
+        player_id       SERIAL, 
         aggression      INT,
         interceptions   INT,
         positioning     INT,
         vision          INT,
         penalties       INT,
-        composure       INT,          
+        composure       INT,  
+        PRIMARY KEY (mentality_id),       
         FOREIGN KEY (player_id) REFERENCES player(player_id) ON DELETE CASCADE ON UPDATE CASCADE
     )
     """
@@ -65,18 +169,49 @@ def fill_tables():
     con = dbapi.connect("host='localhost' dbname='Fifa' user='postgres' password='postgres'")
     cur = con.cursor()
     #QUERIES
-    query_insert_player = """INSERT INTO player VALUES('%s')"""
-    query_insert_player_goalkeeping = """INSERT INTO player_goalkeeping VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s')"""
-    query_insert_player_mentality = """INSERT INTO player_mentality VALUES('%s', '%s', '%s','%s', '%s', '%s', '%s', '%s')"""
+    query_insert_team = """INSERT INTO team (team_id) VALUES(%s)"""
+    query_insert_player = """INSERT INTO player(player_name, date_of_birth, height, weight, overall_rating,potential_rating, best_position, best_overall_rating, value, wage,player_image_url,nationality,team_id ) VALUES( %s, %s, %s, %s, %s, %s,%s,%s, %s, %s, %s, %s,%s)"""
+    query_insert_player_attacking = """INSERT INTO player_attacking(crossing, finishing,heading_accuracy, short_passing, volleys) VALUES(%s, %s, %s, %s, %s)"""
+    query_insert_player_profile = """INSERT INTO player_profile(preferred_foot,weak_foot,skill_moves,international_reputations,work_rate,body_type) VALUES(  %s, '%s', '%s','%s', %s,%s)"""
+    query_insert_player_skills = """INSERT INTO player_skills(dribbling,curve,fk_accuracy,long_passing,ball_control) VALUES(  '%s', '%s', '%s', '%s', '%s')"""
+    query_insert_player_power = """INSERT INTO player_power(shot_power, jumping, stamina,strength,long_shots) VALUES(  '%s', '%s', '%s', '%s', '%s')"""
+    query_insert_player_movement = """INSERT INTO player_movement(acceleration,sprint_speed,agility,reactions_id,balance) VALUES(  '%s', '%s', '%s', '%s', '%s')"""
+    query_insert_player_goalkeeping = """INSERT INTO player_goalkeeping(diving,handling,kicking,positioning,reflexes) VALUES( '%s', '%s', '%s', '%s', '%s')"""
+    query_insert_player_mentality = """INSERT INTO player_mentality(aggression,interceptions,positioning,vision,penalties,composure) VALUES( '%s','%s', '%s', '%s', '%s', '%s')"""
 
-    #CSVs
-    df_players = pd.read_csv("players.csv", usecols=["int_player_id"], dtype=int)
-    df_player_goalkeeping = pd.read_csv("tbl_player_goalkeeping.csv", dtype=int)
-    df_player_mentality = pd.read_csv("tbl_player_mentality.csv", dtype=int)
+    # CSVs
+    df_player_profile = pd.read_csv("./data/tbl_player_profile.csv", usecols=["str_preferred_foot", "int_weak_foot", "int_skill_moves", "int_international_reputations", "str_work_rate", "str_body_type"])
+    df_player_skills = pd.read_csv("./data/tbl_player_skill.csv", dtype=int, usecols=["int_dribbling", "int_curve", "int_fk_accuracy", "int_long_passing", "int_ball_control"])
+    df_player_movement = pd.read_csv("./data/tbl_player_movement.csv", dtype=int, usecols=["int_acceleration", "int_sprint_speed", "int_agility", "int_reactions", "int_balance"])
+    df_player_power = pd.read_csv("./data/tbl_player_power.csv", dtype=int, usecols=["int_shot_power", "int_jumping", "int_stamina", "int_strength", "int_long_shots"])
+    df_player_goalkeeping = pd.read_csv("./data/tbl_player_goalkeeping.csv", dtype=int, usecols=["int_diving", "int_handling", "int_kicking", "int_positioning", "int_reflexes"])
+    df_player_mentality = pd.read_csv("./data//tbl_player_mentality.csv", dtype=int, usecols=["int_aggression", "int_interceptions", "int_positioning", "int_vision", "int_penalties", "int_composure"])
+    df_team = pd.read_csv("./data/tbl_team.csv",usecols=["int_team_id"], dtype=int)
+    df_player = pd.read_csv("./data/n.csv", usecols=["str_player_name", "dt_date_of_birth", "int_height", "int_weight", "int_overall_rating", "int_potential_rating",
+                            "str_best_position", "int_best_overall_rating", "int_value", "int_wage", "str_player_image_url", "int_team_id", "str_nationality"],dtype={"dt_date_of_birth":str})
+    # fill the null team id values with -1 which is no team
+    df_player["int_team_id"] = df_player["int_team_id"].fillna(-1)
+    print(df_player)
+    df_player_attacking = pd.read_csv("./data/tbl_player_attacking.csv", dtype=int, usecols=["int_crossing", "int_finishing", "int_heading_accuracy", "int_short_passing", "int_volleys"])
 
-    #INSERTION
-    df_players.apply(lambda x: insert(query_insert_player, x, cur), axis=1)
-    df_player_goalkeeping.apply(lambda x: insert(query_insert_player_goalkeeping, x, cur), axis=1)
-    df_player_mentality.apply(lambda x: insert(query_insert_player_mentality, x, cur), axis=1)
+    # INSERTION
+    df_team.apply(lambda x: insert(query_insert_team, x, cur), axis=1)
+    df_player.apply(lambda x: insert(query_insert_player, x, cur), axis=1)
+    # con.commit()
+
+    df_player_attacking.apply(lambda x: insert(
+        query_insert_player_attacking, x, cur), axis=1)
+    df_player_profile.apply(lambda x: insert(
+        query_insert_player_profile, x, cur), axis=1)
+    df_player_movement.apply(lambda x: insert(
+        query_insert_player_movement, x, cur), axis=1)
+    df_player_power.apply(lambda x: insert(
+        query_insert_player_power, x, cur), axis=1)
+    df_player_skills.apply(lambda x: insert(
+        query_insert_player_skills, x, cur), axis=1)
+    df_player_goalkeeping.apply(lambda x: insert(
+        query_insert_player_goalkeeping, x, cur), axis=1)
+    df_player_mentality.apply(lambda x: insert(
+        query_insert_player_mentality, x, cur), axis=1)
     con.commit()
     con.close()
